111113829df0d294a0d1ab35afe65bb20da4c40a
VictorRgdz17 <victor.rodriguezl@uanl.edu.mx>
2024-08-15 23:24:00 -0600

    Delete Ejercicio 9.py

--- Ejercicio 9.py
+++
@@ -1,7 +0,0 @@
-def generar_n_caracteres(n, caracter):
-    if len(caracter) != 1:
-        raise ValueError("El segundo argumento debe ser un carácter único.")
-    return caracter * n
-print(generar_n_caracteres(5, "x"))
-print(generar_n_caracteres(3, "#"))
-print(generar_n_caracteres(0, "a"))
